3bcfd7e26eb8319935a7c72f6aca750bfab2e2dc
gsampaiowz <gabrielsampaio1216@gmail.com>
2024-02-08 12:28:56 -0300

    test

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,7 @@
-import Logo from "@/assets/img/logo.png";
-
 const Header = () => {
   return (
     <header className="w-full h-24 flex items-center justify-center bg-white">
-      <img style={{objectPosition: "25% 36%"}} className="object-cover w-48 h-24" src={Logo.src} alt="Logo" />
+      <img style={{objectPosition: "25% 36%"}} className="object-cover w-48 h-24" src="https://drive.google.com/thumbnail?id=1mjiM6ad6SYWcXsnmhqXos8zXmXUjPz2j&sz=w10000" alt="Logo" />
     </header>
   );
 };
